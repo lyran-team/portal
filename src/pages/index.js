18cfb8b6d4535d43bcc44128cab36fe60e975ed7
Diego Arcega <diegoarcega@gmail.com>
2020-01-21 08:55:22 -0300

    feature(list-channels) channels that deliver the message intended by the portal

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -8,13 +7,14 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Canais</h1>
+    <ul>
+      <li>infinitando</li>
+      <li>Danna</li>
+      <li>Abraham</li>
+      <li>Bashar</li>
+      <li>hucolo.org</li>
+    </ul>
   </Layout>
 )
 
