1798bf3394fe17717d81ef84d5f1d997c9c41f7c
Diego Arcega <diegoarcega@gmail.com>
2020-01-21 09:14:42 -0300

    feature(favicon) added icons

--- src/components/layout.js
+++ src/components/layout.js
@@ -24,7 +24,7 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
+    <React.Fragment>
       <Header siteTitle={data.site.siteMetadata.title} />
       <div
         style={{
@@ -35,13 +35,9 @@ const Layout = ({ children }) => {
         }}
       >
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+        <footer>© {new Date().getFullYear()}</footer>
       </div>
-    </>
+    </React.Fragment>
   )
 }
 
